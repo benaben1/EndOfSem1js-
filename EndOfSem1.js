c81fd36d96af617d8679f209dc72fdd7c418d9ca
benaben1 <nabwirebenaa17@gmail.com>
2022-02-17 16:22:39 +0300

    js

---
+++ EndOfSem1.js
@@ -0,0 +1,13 @@
+  function validateForm() {
+        let x = document.forms["form"]["fname"].value;
+        if (x == "") {
+        alert("Name must be filled out");
+        return false;
+        }
+        }
+        
+submit.onclick = function(){
+    alert("Successfully submitted")};
+    
+
+
